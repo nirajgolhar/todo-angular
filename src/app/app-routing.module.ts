1c25f9ef7902b2d071ac38b0d5480c86e71bc76d
nirajgolhar <nsgolhar@gmail.com>
2022-07-22 17:25:59 +0530

    todo app using angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { OtherComponent } from './other/other.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoComponent,
+  },
+  { path: '404', component: OtherComponent },
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
